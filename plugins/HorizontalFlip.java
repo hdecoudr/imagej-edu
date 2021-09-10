a39f9407fa2fb613dd78fa35dc9a19006e749b91
unknown <hdecoudras@gmail.com>
2021-09-10 18:10:05 +0200

    HorizontalFlip + RGBChannel

---
+++ plugins/HorizontalFlip.java
@@ -0,0 +1,28 @@
+import ij.ImagePlus;
+import ij.plugin.filter.PlugInFilter;
+import ij.process.ImageProcessor;
+
+public class HorizontalFlip implements PlugInFilter {
+	
+	@Override
+	public void run(ImageProcessor ip){
+		this.flipH(ip);
+	}
+	
+	public void flipH(ImageProcessor ip){
+		int pxA;
+		
+		for(int j = 0; j < ip.getHeight(); ++j){
+			for(int i = 0; i < ip.getWidth() / 2; ++i){
+				pxA = ip.getPixel(i, j);
+				ip.putPixel(i, j, ip.getPixel(ip.getWidth() - i - 1, j));
+				ip.putPixel(ip.getWidth() - i - 1, j, pxA);
+			}
+		}
+	}
+	
+	@Override
+	public int setup(String arg, ImagePlus imp){
+		return DOES_RGB;
+	}
+}
