0b7166cd6fd38b81bef0a2cfda889eb43091ecb9
unknown <hdecoudras@gmail.com>
2021-09-20 06:35:40 +0200

    ExtractLUT

---
+++ plugins/ExtractLUT.java
@@ -0,0 +1,33 @@
+import ij.IJ;
+import ij.ImagePlus;
+import ij.io.SaveDialog;
+import ij.plugin.filter.PlugInFilter;
+import ij.process.ImageProcessor;
+
+public class ExtractLUT implements PlugInFilter{
+	private String saveLocation;	
+	
+	@Override
+	public void run(ImageProcessor arg0) {
+		this.initSaveDialog();
+		ExtractLUTProcessor elProc = new ExtractLUTProcessor(arg0);
+		elProc.extractLUT();
+		elProc.serialize(this.saveLocation);
+	}
+
+	@Override
+	public int setup(String arg0, ImagePlus arg1) {
+		return DOES_ALL;
+	}
+	
+	private void initSaveDialog(){
+		SaveDialog sd = new SaveDialog("Save LUT ...", null, null);
+		
+		if(sd.getFileName() == null){
+			IJ.error("Plugin cancelled");
+			return;
+		}
+		
+		this.saveLocation = sd.getDirectory() + sd.getFileName();
+	}
+}
