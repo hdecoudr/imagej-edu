374eb7f3496403d92dfcb5e1d871c5628cb9047f
unknown <hdecoudras@gmail.com>
2021-09-23 07:08:13 +0200

    mergeRGBChannels

--- plugins/ArrayTools.java
+++ plugins/ArrayTools.java
@@ -1,7 +1,6 @@
 public final class ArrayTools {
 	public static byte[][] explodeRGBChannels(byte[] rgb) {
 		byte[][] expl = new byte[3][rgb.length / 3];
-		
 		int i = 0;
 		int j = 0;
 		
@@ -29,6 +28,30 @@ public final class ArrayTools {
 		}
 		
 		merg[j] = (((int)rgb[i] & 0xFF) << 0x10 | ((int)rgb[i + 1] & 0xFF) << 0x08 | ((int)rgb[i + 2]) & 0xFF);
+		
+		return merg;
+	}
+	
+	public static byte[] mergeRGBChannels(int[] r, int[] g, int[] b) {
+		if(r.length != g.length || r.length != b.length || g.length != b.length) {
+			return null;
+		}
+		
+		byte[] merg = new byte[r.length * 3];
+		
+		
+		int i = 0;
+		int j = 0;
+		
+		for( ; i < (r.length - 3); ++i, j += 3) {
+			merg[j] = (byte)(r[i] & 0xFF);
+			merg[j + 1] = (byte)(g[i] & 0xFF);
+			merg[j + 2] = (byte)(b[i] & 0xFF);
+		}
+		
+		merg[j] = (byte)(r[i] & 0xFF);
+		merg[j + 1] = (byte)(g[i] & 0xFF);
+		merg[j + 2] = (byte)(b[i] & 0xFF);
 		
 		return merg;
 	}
