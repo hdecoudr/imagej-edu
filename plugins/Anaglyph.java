234a67e7d865cd50e9fa4245a09b940193e6802f
unknown <hdecoudras@gmail.com>
2021-09-25 10:05:01 +0200

    Anaglyph

---
+++ plugins/Anaglyph.java
@@ -0,0 +1,36 @@
+import ij.IJ;
+import ij.ImagePlus;
+import ij.gui.GenericDialog;
+import ij.plugin.filter.PlugInFilter;
+import ij.process.ImageProcessor;
+
+public class Anaglyph implements PlugInFilter{
+	private int shift;
+	
+	@Override
+	public void run(ImageProcessor arg0) {
+		this.initGenericDialog();
+		
+		AnaglyphProcessor anaProc = new AnaglyphProcessor(arg0, this.shift);
+		ImagePlus imRes = anaProc.anaglyph();
+		imRes.show();
+	}
+
+	@Override
+	public int setup(String arg0, ImagePlus arg1) {
+		return DOES_RGB;
+	}
+	
+	private void initGenericDialog() {
+		GenericDialog gd = new GenericDialog("Shift", IJ.getInstance());
+		gd.addSlider("Shift", 0.0, 20.0, 2.0);
+		gd.showDialog();
+		
+		if(gd.wasCanceled()) {
+			IJ.error("PlugIn cancelled");
+			return;
+		}
+		
+		this.shift = (int)gd.getNextNumber();
+	}
+}
